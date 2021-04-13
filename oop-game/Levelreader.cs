a8207256177fe82420353c80348ae8f5c1e5501e
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2021-04-13 12:23:30 +0200

    map and moving char

---
+++ oop-game/Levelreader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace oop_game
+{
+    class Levelreader
+    {
+        public static string[,] ReadFileToArray(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            string firstLine = lines[0];
+            int rows = lines.Length;
+            int cols = firstLine.Length;
+            string[,] grid = new string[rows, cols];
+            for (int y = 0; y < rows; y++)
+            {
+                string line = lines[y];
+                for (int x = 0; x < cols; x++)
+                {
+                    char currentChar = line[x];
+                    grid[y, x] = currentChar.ToString();
+                }
+            }
+            return grid;
+        }
+
+    }
+}
---
+++ oop-game/World.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_game
+{
+    class World
+    {
+        public string[,] Grid;
+        private int Rows;
+        private int Cols;
+
+        public World(string[,] grid)
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Cols = Grid.GetLength(1);
+        }
+        public void Draw()
+        {
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int x = 0; x < Cols; x++)
+                {
+                    string element = Grid[y, x];
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(element);
+                }
+            }
+        }
+    }
+}
