7400479a19564b6010dcf33dca309b25562ec414
Farhad <farhad.tavakoli81@yahoo.com>
2021-04-14 14:03:10 +0200

    Console Menu

--- oop-game/Program.cs
+++ oop-game/Program.cs
@@ -13,10 +13,62 @@ namespace oop_game
             _gameSession = new GameSession();
 
             buffer = _gameSession.currentMaze.Buffer;
-            Console.CursorVisible = false;
-            GameLoop();
+            Console.CursorVisible = true;
+            menu();
+            //GameLoop();
         }
-        private static void PlayerInput()
+
+
+        public static void  menu()
+        {
+            bool validChoise;
+            int selection;
+                Console.ForegroundColor = ConsoleColor.Green;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Spela ");
+                Console.WriteLine("[2] Instraktioner ");
+                Console.WriteLine("[3] Avsluta");
+                Console.Write("Välj [1..3] :");
+
+                validChoise = int.TryParse(Console.ReadLine(), out selection);
+                if (!validChoise)
+                    Console.Beep(1000, 50);
+            } while (!validChoise);
+
+            switch(selection)
+            {
+                case 1:
+                        GameLoop();
+                    break;
+                case 2:
+                    viewInstruktion();
+                    menu();
+                    break;
+                case 3:
+                    exit();
+                    break;
+                default:
+                    Console.Beep(1000, 50);
+                    menu();
+                    break;
+            }
+        }
+        public static void viewInstruktion()
+        {
+            Console.WriteLine("Instruktioner...");
+            Console.ReadKey();
+        }
+
+        public static void exit()
+        {
+            Console.WriteLine("Tryck på en knapp");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+            private static void PlayerInput()
         {
             ConsoleKeyInfo keypress = Console.ReadKey(true);
             ConsoleKey key = keypress.Key;
@@ -46,12 +98,20 @@ namespace oop_game
                     _gameSession.Move(_gameSession.currentPlayer.X + 1, _gameSession.currentPlayer.Y);
                     
                     break;
-                default:
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                        menu();
+                        break;
+                    default:
                     break;
             }
         }
         private static void GameLoop()
         {
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.ResetColor();
             FastDraw(buffer);
             while (true)
             {
