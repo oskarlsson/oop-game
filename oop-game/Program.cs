54ca443e3fe148fa730382821085fd4eb69d67d0
Robin Ferm <robinferm@live.com>
2021-04-16 16:16:15 +0200

    center loading screen

--- oop-game/Program.cs
+++ oop-game/Program.cs
@@ -447,10 +447,11 @@ namespace oop_game
             FrameDimension Dimension = new FrameDimension(Picture.FrameDimensionsList[0x0]);
             int FrameCount = Picture.GetFrameCount(Dimension);
             int Left = Console.WindowLeft, Top = Console.WindowTop;
-            char[] Chars = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+            char[] Chars = { '#', '#', '@', '%', '=', '+', '*', ':', '-', ' ', ' ' };
             Picture.SelectActiveFrame(Dimension, 0x0);
             for (int i = 0x0; i < Picture.Height; i++)
             {
+                Console.Write("\t\t\t\t\t\t      ");
                 for (int x = 0x0; x < Picture.Width; x++)
                 {
                     Color Color = ((Bitmap)Picture).GetPixel(x, i);
